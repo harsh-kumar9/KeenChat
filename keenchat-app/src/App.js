bf02deb06828314b3deb459a2e9b83689ac4eab6
Huayin Luo <hclover2003@gmail.com>
2023-11-28 15:22:48 -0500

    add routes for 6 conditions

--- keenchat-app/src/App.js
+++ keenchat-app/src/App.js
@@ -3,15 +3,44 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Conversation from "./components/Conversation";
 import AudioComponent from "./components/AudioComponent";
 
+// 6 routes for 6 different conversation types
+// 1. voice-voice: voice bot message, voice bot backchannel
+// 2. voice-text: voice bot message, text bot backchannel
+// 3. voice-none: voice bot message, no backchannel
+// 4. text-voice: text bot message, voice bot backchannel
+// 5. text-text: text bot message, text bot backchannel
+// 6. text-none: text bot message, no backchannel
+
 function App() {
   return (
     <div>
       <Router basename="/KeenChat">
         <Routes>
           <Route path="/" element={<Conversation />} />
-          <Route path="/static" element={<Conversation type="static" />} />
-          <Route path="/animated" element={<Conversation type="animated" />} />
-          <Route path="/audio" element={<AudioComponent />} />
+          <Route
+            path="/voice-voice"
+            element={<Conversation type="voice-voice" />}
+          />
+          <Route
+            path="/voice-text"
+            element={<Conversation type="voice-text" />}
+          />
+          <Route
+            path="/voice-none"
+            element={<Conversation type="voice-none" />}
+          />
+          <Route
+            path="/text-voice"
+            element={<Conversation type="text-voice" />}
+          />
+          <Route
+            path="/text-text"
+            element={<Conversation type="text-text" />}
+          />
+          <Route
+            path="/text-none"
+            element={<Conversation type="text-none" />}
+          />
         </Routes>
       </Router>
     </div>
