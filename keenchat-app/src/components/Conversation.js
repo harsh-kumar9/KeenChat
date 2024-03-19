d0531ac76515986fa6c3f278bc53bba284fa7854
Huayin Luo <hclover2003@gmail.com>
2024-03-18 21:22:32 -0400

    fixed bugs - working

--- keenchat-app/src/components/Conversation.js
+++ keenchat-app/src/components/Conversation.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
-
 import { convoActions } from "../reducers/convoSlice";
 import { emojiActions } from "../reducers/emojiSlice";
 import { ask, react, ask_backchannel } from "../actions/langchainAction";
 import "../styles/Conversation.css";
-
 import { TextField, Button } from "@mui/material";
 import ReactionEmoji from "./ReactionEmoji";
 import bot_talk from "../assets/bot-talk.png";
@@ -17,11 +15,14 @@ import * as sdk from "microsoft-cognitiveservices-speech-sdk";
 var subscriptionKey = process.env.REACT_APP_AZURE_SPEECH_KEY;
 var serviceRegion = process.env.REACT_APP_AZURE_SPEECH_REGION; // e.g., "westus"
 
-// Conversation component
-// main: "voice" or "text"
-// backchannel: "voice", "text", or "none"
-// inputType: "text" or "voice" (for user input, not implemented yet
+/*
+Conversation component
+main: "voice" or "text"
+backchannel: "voice", "text", or "none"
+inputType: "text" or "voice" (for user input, not implemented yet
+*/
 const Conversation = ({ main, backchannelType, inputType }) => {
+  // TODO: save data to database
   // global states
   const dispatch = useDispatch();
   const langchain = useSelector((state) => state.langchain);
@@ -32,12 +33,14 @@ const Conversation = ({ main, backchannelType, inputType }) => {
   const [isTyping, setIsTyping] = useState(false);
   const [selectedBackchannel, setSelectedBackchannel] = useState("");
   const [selectedEmoji, setSelectedEmoji] = useState("");
-  const [synthesizer, setSynthesizer] = useState(null);
+  // const [synthesizer, setSynthesizer] = useState(null);
   const [userInput, setUserInput] = useState("");
   const [lastBotMsg, setLastBotMsg] = useState("");
+  const [voiceName, setVoiceName] = useState("en-US-AriaNeural");
 
+  const lst = `specific list: "mmm", "yeah", "right", "wow", "okay", "sure", "hmm", "uh-huh", "gotcha", "cool".`;
   const backchannel_prompt = `  
-  Based on the conversation context provided by the user, select and generate a single-word backchannel response from this specific list: "mhm", "yeah", "right", "wow", "okay", "sure", "hmm", "uh-huh", "gotcha", "cool". These words are commonly used to indicate active listening and understanding in a conversation. Ensure that the chosen word is contextually appropriate, reflecting attentiveness or acknowledgment relevant to the specific part of the conversation provided. The response should consist solely of the selected backchannel word, without any additional content. If no backchannel word is an appropriate response, please return "...". Some examples include, if the input message is "So I’ve always felt very scared", the response could be "hmm". If the input message is "Yeah you know that feeling when you’re so pumped up" the response could be "yeah!".
+  Based on the conversation context provided by the user, select and generate a single-word backchannel response without any special characters or punctuation like exclamation marks. These words are commonly used to indicate active listening and understanding in a conversation. Ensure that the chosen word is contextually appropriate, reflecting attentiveness or acknowledgment relevant to the specific part of the conversation provided. The response should consist solely of the selected backchannel word, without any additional content. If no backchannel word is an appropriate response, please return "...". Some examples include, if the input message is "So I’ve always felt very scared", the response could be "hmm". If the input message is "Yeah you know that feeling when you’re so pumped up" the response could be "yeah!".
   `;
 
   // constants
@@ -83,6 +86,11 @@ const Conversation = ({ main, backchannelType, inputType }) => {
       serviceRegion
     );
     speechConfig.speechRecognitionLanguage = "en-US";
+    // fixes the issue of speech recognition restarting after 2 seconds of silence
+    speechConfig.setProperty(
+      sdk.PropertyId.Speech_SegmentationSilenceTimeoutMs,
+      "5000"
+    );
     const audioConfig = sdk.AudioConfig.fromDefaultMicrophoneInput();
     const newRecognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
 
@@ -90,14 +98,14 @@ const Conversation = ({ main, backchannelType, inputType }) => {
     newRecognizer.recognizing = (s, e) => {
       console.log(`RECOGNIZING: Text=${e.result.text}`);
       if (backchannelType === "voice") {
-        generateBackchannel(e.result.text, 3, 1);
+        console.log("backchannel voice");
+        generateBackchannelBot(e.result.text);
       }
     };
-
     newRecognizer.recognized = (s, e) => {
       if (e.result.reason === sdk.ResultReason.RecognizedSpeech) {
         console.log(`RECOGNIZED final: Text=${e.result.text}`);
-        setInputValue(e.result.text); // Final update to inputValue
+        processAudioMessage(e.result.text);
       }
     };
 
@@ -115,7 +123,6 @@ const Conversation = ({ main, backchannelType, inputType }) => {
         () => {
           console.log("Recognition stopped");
           setIsListening(false);
-          processMessage(); // Process the final message
         },
         (error) => {
           console.error("Error stopping recognition:", error);
@@ -127,10 +134,7 @@ const Conversation = ({ main, backchannelType, inputType }) => {
 
   const toggleContinuousSpeechRecognition = () => {
     if (isListening) {
-      // TODO: doesn't work if user stops listening before speech is recognized
-      // ie. need to wait ~0.5s after you stop talking before you can press "stop listening"
-      // how to make sure speech is recognized before stopping?
-      console.log("stopping speech recognition");
+      // Wait for 0.5 seconds before sending stopcontinuousrecognition
       stopContinuousSpeechRecognition();
     } else {
       console.log("starting speech recognition");
@@ -139,45 +143,53 @@ const Conversation = ({ main, backchannelType, inputType }) => {
   };
 
   // Synthesizer for TTS (text-to-speech)
-  useEffect(() => {
+  // useEffect(() => {
+  //   const speechConfig = sdk.SpeechConfig.fromSubscription(
+  //     subscriptionKey,
+  //     serviceRegion
+  //   );
+  //   speechConfig.speechSynthesisVoiceName = voiceName;
+  //   speechConfig.speechSynthesisLanguage = "en-US";
+  //   const audioConfig = sdk.AudioConfig.fromSpeakerOutput();
+  //   const newSynthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
+
+  //   // Set the synthesizer in state
+  //   setSynthesizer(newSynthesizer);
+
+  //   // Cleanup function to dispose of the synthesizer when the component unmounts
+  //   return () => {
+  //     if (newSynthesizer) {
+  //       newSynthesizer.close();
+  //     }
+  //   };
+  // }, []);
+
+  const synthesizeSpeech = (text) => {
     const speechConfig = sdk.SpeechConfig.fromSubscription(
       subscriptionKey,
       serviceRegion
     );
+    speechConfig.speechSynthesisVoiceName = voiceName;
+    speechConfig.speechSynthesisLanguage = "en-US";
     const audioConfig = sdk.AudioConfig.fromSpeakerOutput();
     const newSynthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
-
-    // Set the synthesizer in state
-    setSynthesizer(newSynthesizer);
-
-    // Cleanup function to dispose of the synthesizer when the component unmounts
-    return () => {
-      if (newSynthesizer) {
-        newSynthesizer.close();
-      }
-    };
-  }, []);
-
-  const synthesizeSpeech = (text) => {
-    if (synthesizer) {
-      synthesizer.speakTextAsync(
-        text,
-        (result) => {
-          if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
-            console.log("Speech synthesis completed.");
-          } else {
-            console.error("Speech synthesis canceled, " + result.errorDetails);
-          }
-          // Do not close the synthesizer here
-        },
-        (err) => {
-          console.error("Error occurred during speech synthesis: " + err);
-          // Do not close the synthesizer here
+    newSynthesizer.speakTextAsync(
+      text,
+      (result) => {
+        if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+          console.log("Speech synthesis completed.");
+          newSynthesizer.close();
+        } else {
+          console.error("Speech synthesis canceled, " + result.errorDetails);
+          newSynthesizer.close();
         }
-      );
-    } else {
-      console.error("Synthesizer not initialized.");
-    }
+        // Do not close the synthesizer here
+      },
+      (err) => {
+        console.error("Error occurred during speech synthesis: " + err);
+        // Do not close the synthesizer here
+      }
+    );
   };
 
   // Get chat history from JSON
@@ -202,56 +214,108 @@ const Conversation = ({ main, backchannelType, inputType }) => {
   };
 
   const processMessage = () => {
-    if (inputValue.trim() !== "") {
-      const inputJSON = JSON.parse(JSON.stringify(langchain.inputJSON));
-      inputJSON.chat_history += historyToText(
+    console.log("processing message");
+    let typedMessage = inputValue;
+    console.log("typed message", typedMessage);
+    setLastBotMsg("...");
+
+    // if user inputType is text, check that textbox is not empty
+    if (inputType === "text") {
+      if (typedMessage.length === 0) {
+        return;
+      }
+    }
+
+    console.log("input value", inputValue);
+    const inputJSON = JSON.parse(JSON.stringify(langchain.inputJSON));
+    inputJSON.chat_history += historyToText(
+      langchain.bot_identifier,
+      langchain.human_identifier,
+      langchain.history
+    );
+    inputJSON.inputs = inputValue;
+    console.log(inputJSON);
+    // dispatch convo reset
+    dispatch(convoActions.reset());
+
+    // get bot message
+    dispatch(
+      ask(
+        langchain.prompt,
         langchain.bot_identifier,
         langchain.human_identifier,
-        langchain.history
-      );
-      inputJSON.inputs = inputValue;
+        inputJSON
+      )
+    ).then((botResponse) => {
+      let index = 0;
+      let msg = botResponse["text"];
+      const intervalId = setInterval(() => {
+        setLastBotMsg(msg.slice(0, index + 1));
+        index++;
+        if (index === msg.length) clearInterval(intervalId);
+      }, 2); // Adjust the delay between characters as needed
 
-      // dispatch convo reset
-      dispatch(convoActions.reset());
+      // text-to-speech
+      if (main === "voice") {
+        console.log("Synthesizing", msg);
+        synthesizeSpeech(msg);
+      }
+    });
+  };
 
-      // get bot message
-      dispatch(
-        ask(
-          langchain.prompt,
-          langchain.bot_identifier,
-          langchain.human_identifier,
-          inputJSON
-        )
-      ).then((botResponse) => {
-        // reset input value to empty
-        setInputValue("");
-        let index = 0;
-        let msg = botResponse["text"];
-        const intervalId = setInterval(() => {
-          setLastBotMsg(msg.slice(0, index + 1));
-          index++;
-          if (index === msg.length) clearInterval(intervalId);
-        }, 10); // Adjust the delay between characters as needed
+  const processAudioMessage = (userMsg) => {
+    console.log("processing audio message");
+    setLastBotMsg("...");
+    const inputJSON = JSON.parse(JSON.stringify(langchain.inputJSON));
+    inputJSON.chat_history += historyToText(
+      langchain.bot_identifier,
+      langchain.human_identifier,
+      langchain.history
+    );
+    inputJSON.inputs = userMsg;
+    console.log(inputJSON);
+    // dispatch convo reset
+    dispatch(convoActions.reset());
 
-        // text-to-speech
-        if (main === "voice") {
-          synthesizeSpeech(botResponse["text"]);
-        }
-      });
-    }
+    // get bot message
+    dispatch(
+      ask(
+        langchain.prompt,
+        langchain.bot_identifier,
+        langchain.human_identifier,
+        inputJSON
+      )
+    ).then((botResponse) => {
+      let index = 0;
+      let msg = botResponse["text"];
+
+      // text-to-speech
+      if (main === "voice") {
+        console.log("Synthesizing", msg);
+        synthesizeSpeech(msg);
+      }
+
+      // display bot message character by character
+      const intervalId = setInterval(() => {
+        setLastBotMsg(msg.slice(0, index + 1));
+        index++;
+        if (index === msg.length) clearInterval(intervalId);
+      }, 2); // Adjust the delay between characters as needed
+    });
   };
 
   // Generate bot response after user submits message
   const handleMessageSubmit = (e) => {
-    setSelectedBackchannel("");
-    setLastBotMsg("");
-    setIsTyping(false);
+    setSelectedBackchannel(""); // Clear backchannel
+    setLastBotMsg(""); // Clear last bot message
+    setIsTyping(false); // Set isTyping to false (changes bot avatar)
     e.preventDefault();
-    processMessage();
+    processMessage(); // Process message
   };
 
   // Generate backchannel while user types
   const handleMessageChange = async (e) => {
+    console.log("handle message change");
     const message = e.target.value;
     setInputValue(message);
     setIsTyping(true);
@@ -269,12 +333,17 @@ const Conversation = ({ main, backchannelType, inputType }) => {
 
   const generateBackchannelBot = async (message) => {
     console.log("checking for backchannel...");
-    if (backchannelType != "none" && message.length % 10 == 0) {
+    if (backchannelType != "none" && message.length % 20 == 0) {
       dispatch(ask_backchannel(backchannel_prompt, message)).then(
         (botResponse) => {
           console.log("backchannel", botResponse);
           const backchannelText = botResponse;
           let index = 0;
+          // text-to-speech
+          if (backchannelType == "voice") {
+            synthesizeSpeech(backchannelText);
+          }
+
           const backchannelInterval = setInterval(() => {
             setSelectedBackchannel(backchannelText.slice(0, index));
             index++;
@@ -282,49 +351,8 @@ const Conversation = ({ main, backchannelType, inputType }) => {
               clearInterval(backchannelInterval);
             }
           }, 100);
-
-          // text-to-speech
-          if (backchannelType == "voice") {
-            synthesizeSpeech(backchannelText);
-          }
         }
       );
-    }
-  };
-
-  const generateBackchannel = (
-    message,
-    msgInterval = 7,
-    msgProbability = 0.5
-  ) => {
-    console.log("checking for backchannel...");
-    if (backchannelType != "none") {
-      const msgLength = message.length;
-      const randBinary = Math.random();
-      if ((msgLength % msgInterval == 0) & (randBinary < msgProbability)) {
-        // Select a random backchannel from the backchannels array
-        // const backchannel =
-        //   backchannels[Math.floor(Math.random() * backchannels.length)];
-        const randomIndex = Math.floor(Math.random() * backchannels.length);
-        const backchannel = backchannels[randomIndex];
-        const backchannelText = `${backchannel}...`;
-        setSelectedBackchannel(backchannelText);
-        console.log("backchannel: " + backchannel);
-
-        // text-to-speech
-        if (backchannelType == "voice") {
-          synthesizeSpeech(backchannel);
-        }
-        // Use setInterval to update the backchannel one letter at a time
-        let index = 0;
-        const backchannelInterval = setInterval(() => {
-          setSelectedBackchannel(backchannelText.slice(0, index));
-          index++;
-          if (index > backchannelText.length) {
-            clearInterval(backchannelInterval);
-          }
-        }, 100);
-      }
     }
   };
 
@@ -342,6 +370,8 @@ const Conversation = ({ main, backchannelType, inputType }) => {
   useEffect(() => {
     scrollToBottom();
   }, [lastBotMsg]);
+
+  // frontend
   return (
     <div className="conversation">
       <h1>KeenChat</h1>
@@ -376,13 +406,23 @@ const Conversation = ({ main, backchannelType, inputType }) => {
       </div>
 
       <div className="bot-avatar-backchannel">
-        <img src={isTyping ? bot_listen : bot_talk} id="bot_avatar_img" />
+        {inputType === "text" ? (
+          <img src={isTyping ? bot_listen : bot_talk} id="bot_avatar_img" />
+        ) : (
+          <img src={isListening ? bot_listen : bot_talk} id="bot_avatar_img" />
+        )}
         <span>Bot </span>
 
         {backchannelType === "text" ? (
           <div className="backchannel_container" style={{ marginLeft: "20px" }}>
-            <p style={{ display: "inline" }}>{selectedBackchannel}</p>
-            <p style={{ display: "inline", fontStyle: "normal" }}>
+            <p style={{ display: "inline" }}>{selectedBackchannel} </p>
+            <p
+              style={{
+                display: "inline",
+                fontStyle: "normal",
+                color: "transparent",
+              }}
+            >
               {selectedEmoji}
             </p>
           </div>
@@ -390,27 +430,46 @@ const Conversation = ({ main, backchannelType, inputType }) => {
       </div>
 
       {inputType === "text" ? (
-        <form
-          onSubmit={handleMessageSubmit}
-          className="message-form"
-          style={{ marginTop: "20px" }}
-        >
+        <form onSubmit={handleMessageSubmit} className="message-form">
           <TextField
             value={inputValue}
             onChange={handleMessageChange}
             label="Type a message"
             className="message-input"
             autoComplete="off"
+            style={{ borderRadius: "10px 0px 0px 10px" }}
           />
-          <Button type="submit" variant="contained" className="send-button">
+          <Button
+            type="submit"
+            variant="contained"
+            className="send-button"
+            style={{
+              borderRadius: "0px 10px 10px 0px",
+              boxShadow: "none",
+              background: "black",
+              color: "white",
+            }}
+          >
             Send
+            {/* <i className="fa-solid fa-paper-plane"></i> */}
           </Button>
         </form>
       ) : (
         <Button
           className="voice-input-button"
           onClick={toggleContinuousSpeechRecognition}
+          style={{
+            fontSize: "14px",
+            background: isListening ? "red" : "black",
+            color: "white",
+            marginTop: "20px",
+          }}
+          disableRipple
         >
+          <i
+            className="fa-solid fa-microphone"
+            style={{ marginRight: "10px" }}
+          ></i>
           {isListening ? "Stop Listening" : "Start Listening"}
         </Button>
       )}
